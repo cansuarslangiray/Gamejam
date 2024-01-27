c1bfe02302db6f90109a7a1b1dad2e2f9ec599b4
cansuarslangiray <cansuarslangiray8@gmail.com>
2024-01-27 07:06:47 +0300

    fixed problem in level1

--- Assets/Scripts/Level1/PlayerController.cs
+++ Assets/Scripts/Level1/PlayerController.cs
@@ -8,38 +8,53 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     public float turnSpeed;
-    private float targertPos=0;
-    public int health =1;
+    private float targertPos = 0;
+    public int health = 1;
     public int currentScene = 1;
     public GameObject deadUi;
+    private bool _canMove;
 
-    
+
     void PlayerMovement()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
             if (transform.position.x > -4)
             {
-                targertPos -=4;
+                _canMove = true;
+                targertPos -= 4;
                 transform.DORotate(new Vector3(0, 0, 45), 0.3f).OnComplete(() =>
                 {
                     transform.DORotate(new Vector3(0, 0, 0), 0.3f);
                 });
+            }
+            else
+            {
+                _canMove = false;
             }
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
             if (transform.position.x < 4)
             {
-                targertPos +=4;
+                _canMove = true;
+                targertPos += 4;
                 transform.DORotate(new Vector3(0, 0, -45), 0.3f).OnComplete(() =>
                 {
                     transform.DORotate(new Vector3(0, 0, 0), 0.3f);
                 });
             }
+            else
+            {
+                _canMove = false;
+            }
         }
 
-        transform.position = Vector3.MoveTowards(transform.position,new Vector3(targertPos,transform.position.y,0),turnSpeed);
+        if (_canMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                new Vector3(targertPos, transform.position.y, 0), turnSpeed);
+        }
     }
 
     private void Update()
@@ -49,19 +64,21 @@ public class PlayerController : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-         health -= damage;
-        
+        health -= damage;
+
         if (health <= 0)
         {
             PauseGame();
             deadUi.SetActive(true);
         }
     }
-    void PauseGame ()
+
+    void PauseGame()
     {
         Time.timeScale = 0;
     }
-    void ResumeGame ()
+
+    void ResumeGame()
     {
         Time.timeScale = 1;
     }
@@ -77,5 +94,4 @@ public class PlayerController : MonoBehaviour
     {
         health += healthPoint;
     }
-    
 }
